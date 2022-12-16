8eab50090d7e1b5baae45685f0de81cf35e1ccbe
vruizr <118493922+vruizr@users.noreply.github.com>
2022-12-16 00:56:56 -0600

    prueba contador

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,19 @@
-let c=0onta
+let contador = 0;
+
+const valor= document.querySelector("#valor");
+const botones = document.querySelectorAll(".boton")
+
+botones.forEachi(boton => {
+    boton.addEventListener["click", funcion(e){
+
+        const estilos = e.currentTarget.classList;
+
+        if(estilos.contains("Disminuir")) {
+            contador = contador-3.5;
+        }
+        else(estilos.contains("Aumentar")) {
+            contador = contador+3.5;
+        }
+            valor.textContent=contador;
+    }]
+})
